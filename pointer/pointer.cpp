942405e2d3feda0f197ceedd1d1d97a47c4792ed
ijaamm <ijammusyaf@gmail.com>
2023-05-10 15:24:47 +0700

    class mahasiswa

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -6,24 +6,3 @@ public:
 	int nim;
 	void showNim(); //deklarasi method
 };
-
-void mahasiswa::showNim() { //implementasi method di luar class
-	cout << "no induk = " << nim << endl;
-}
-
-int main()
-{
-	mahasiswa mhs{ 1 }; //objek mhs 
-	mhs.showNim(); //member access operator
-
-	mahasiswa& ref = mhs; //pointer refereces refmhs
-	ref.nim = 2; //number access operator
-	mhs.showNim();
-
-	mahasiswa* pMhs = &mhs; //pointer deferece pMhs
-	pMhs->nim = 3;  //arrow operator
-	mhs.showNim();
-	system("pause");
-	return 0;
-
-}
