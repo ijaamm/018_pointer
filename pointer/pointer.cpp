d7cb727a17915f16731a3bd327470720a2e06298
ijaamm <ijammusyaf@gmail.com>
2023-05-10 15:25:54 +0700

    pointer

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -11,3 +11,19 @@ void mahasiswa::showNim() { //implementasi method di luar class
 	cout << "no induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 }; //objek mhs 
+	mhs.showNim(); //member access operator
+
+	mahasiswa& ref = mhs; //pointer refereces refmhs
+	ref.nim = 2; //number access operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer deferece pMhs
+	pMhs->nim = 3;  //arrow operator
+	mhs.showNim();
+	system("pause");
+	return 0;
+
+}
